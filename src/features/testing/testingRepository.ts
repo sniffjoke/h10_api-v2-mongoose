b6d23d38be0f722521f137fadbd5e52e0d485db4
sniffjoke <sniffjoke@gmail.com>
2024-09-08 21:51:13 +0200

    add env on vercel

--- src/features/testing/testingRepository.ts
+++ src/features/testing/testingRepository.ts
@@ -1,34 +1,3 @@
-// import {
-//     blogCollection,
-//     commentCollection,
-//     deviceCollection,
-//     postCollection,
-//     tokenCollection,
-//     userCollection
-// } from "../db/mongo-db";
-//
-//
-// export const testingRepository = {
-//     async deleteAll() {
-//         const blogs = await blogCollection.deleteMany()
-//         const posts = await postCollection.deleteMany()
-//         const users = await userCollection.deleteMany()
-//         const comments = await commentCollection.deleteMany()
-//         const tokens = await tokenCollection.deleteMany()
-//         const devices = await deviceCollection.deleteMany()
-//         // const rates = await rateLimitCollection.deleteMany()
-//         return {
-//             blogs,
-//             posts,
-//             users,
-//             comments,
-//             tokens,
-//             devices,
-//             // rates
-//         }
-//     },
-// }
-
 import {blogModel} from "../../models/blogsModel";
 import {postModel} from "../../models/postsModel";
 import {userModel} from "../../models/usersModel";
