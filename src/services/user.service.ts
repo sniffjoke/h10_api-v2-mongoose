b2712b96935789ba2f023a42860039f44501fe3e
sniffjoke <sniffjoke@gmail.com>
2024-09-09 00:11:18 +0200

    fix home-task for tests

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -139,7 +139,7 @@ class UserService {
     async updatePassword(password: string, recoveryCode: string) {
         const user = await this.users.findOne({'emailConfirmation.confirmationCode': recoveryCode})
         if (!user) {
-            throw ApiError.BadRequest('Юзер не найден', 'incorrect')
+            throw ApiError.BadRequest('Юзер не найден', 'recoveryCode')
         }
         const emailExists = await usersRepository.getUserByEmail(user.email)
         if (!emailExists) {
